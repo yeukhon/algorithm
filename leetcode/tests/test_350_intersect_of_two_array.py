4650e887b45fd6d90b1ad0a8ae99c47474e03fed
Yeuk Hon Wong <yeukhon@acm.org>
2018-01-14 01:09:12 -0500

    [leetcode] - 350 intersect of two arrays (buggy)
    This version is buggy for cases such as this:
        [1, 2] and [1, 1]
    Depending on which one is selected as shorter and longer,
    the answer will become either [1], or [1, 1].
    
    We can fix this using weight. Build a binary tree as we sort
    the array, and each number in the tree has a weight. Each
    recurrence of the number means minus one from the number's weight.
    If the weight becomes zero and yet we still find another recurrence,
    this means we have exhaust all the recurrence of that number in
    the other list, so the number cannot be added to the intersection.

---
+++ leetcode/tests/test_350_intersect_of_two_array.py
@@ -0,0 +1,32 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import unittest
+from leetcode.s350_intersect_of_two_array import Solution
+
+class Test_350_Intersect_of_Two_Array(unittest.TestCase):
+
+    def _intersect(self, a1, a2):
+        solution = Solution()
+        result = solution.intersect(a1, a2)
+        return result
+
+    def test_12_13_returns_1(self):
+        self.assertEqual(
+            self._intersect([1,2], [1,3]), [1])
+
+    def test_10_45_returns_empty_list(self):
+        self.assertEqual(
+            self._intersect([1,0], [4,5]), [])
+
+    def test_12221_22_returns_22(self):
+        self.assertEqual(
+            self._intersect([1,2,2,1], [2,2]), [2, 2])
+
+    def test_12_11_returns_1(self):
+        self.assertEqual(
+            self._intersect([1,2], [1,1]), [1])
+
+if __name__ == "__main__":
+    unittest.main()
