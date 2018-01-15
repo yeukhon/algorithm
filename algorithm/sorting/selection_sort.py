4f1caaf5acd71a0fa182423b2b5159295a58484b
Yeuk Hon Wong <yeukhon@acm.org>
2018-01-15 00:19:29 -0500

    [sorting] - add merge sort (in-place and non in-place)
    In order to support the in-place merge sort, I added
    ``selection_sort_2`` which accepts start and end range.

--- algorithm/sorting/selection_sort.py
+++ algorithm/sorting/selection_sort.py
@@ -13,7 +13,7 @@ import sys
 if sys.version_info[0] < 3:
     range = xrange
 
-def selection_sort(arr):
+def selection_sort(arr, start=None, end=None):
     """Return a sorted array in-place in ascending order
     by replacing each element in the array with the smallest
     number after its position.
@@ -28,11 +28,38 @@ def selection_sort(arr):
     [1, 2, 3]
 
     """
-
     # compare last element with itself is a waste, so we can
     # i up to length of array - 1
     for i in range(0, len(arr) - 1):
         for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+def selection_sort_2(arr, start=None, end=None):
+    """An enhanced selection sort with the ability to
+    specify the range to sort.
+
+    >>> selection_sort([])
+    []
+    >>> selection_sort([1])
+    [1]
+    >>> selection_sort([2,1])
+    [1, 2]
+    >>> selection_sort([1,2,3])
+    [1, 2, 3]
+
+    """
+
+    if start is None:
+        start = 0
+    if end is None:
+        end = len(arr) - 1
+
+    # compare last element with itself is a waste, so we can
+    # i up to length of array - 1
+    for i in range(start, end):
+        for j in range(i, end+1):
             if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
     return arr
